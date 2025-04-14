37ccf1781fc4673f9ef9d6ee2fb3bcbf36f6c484
NjugunaKelvin <njugunak349@gmail.com>
2025-04-14 18:05:15 +0300

    University file

--- Student.java
+++ Student.java
@@ -1,19 +1,42 @@
+import java.util.ArrayList;
+
 public class Student {
     private String studentName;
-    private String studentRegistration;
-    private char Gender;
+    private int ID;
+    private ArrayList<Course> enrolledCourses;
 
     // constructor
-    public Student(String studentName, String studentRegistration) {
+    public Student(String studentName, int ID) {
         this.studentName = studentName;
-        this.studentRegistration = studentRegistration;
+        this.ID = ID;
     }
 
-    // add students
-    public addStudents() {
+    // enrolled courses
+    public void enrolledCourses(Course course) {
+        enrolledCourses.add(course);
+        System.out.println(course.getCourseName() + " has been added.");
+    }
+
+    // fees
+    public int calculateTotalFees() {
+        int total = 0;
+        for(Course course : enrolledCourses) {
+            total += course.getCourseFee();
+        }
+        return total;
+    }
+
+    // show details
+    public void showDetails() {
+        System.out.println("Name: " + studentName);
+        System.out.println("ID: " + ID);
+        System.out.println("-------ENROLLED COURSES--------");
+        for(Course course : enrolledCourses) {
+            System.out.println("==> " + course);
+        }
+        System.out.println("Total fees: " + calculateTotalFees());
+    }
 
     }
 
     
-
-}
--- University.java
+++ University.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class University {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Course> courses = new ArrayList<>();
+
+        System.out.println("Welcome to the University Management System.");
+
+        while (true) {
+            System.out.println("\n ------MENU------");
+            System.out.println("1. Register Student");
+            System.out.println("2. Add Course");
+            System.out.println("3. Enroll student in a course");
+            System.out.println("4. View all Students");
+            System.out.println("5. Exit");
+
+            System.out.println("Choose an option: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Enter the student name.");
+                    String studentName = scanner.nextLine();
+
+                    System.out.println("Enter students ID: ");
+                    int ID = scanner.nextInt();
+                    students.add(new Student(studentName, ID));
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+
+    }
 }
